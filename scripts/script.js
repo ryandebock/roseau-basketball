3a62e85865e123be7ca0b517a091b9a61659568d
Ryan DeBock <103005477+ryandebock@users.noreply.github.com>
2026-01-11 17:39:52 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -11,14 +11,31 @@ const toggle = document.getElementById('toggle');
 const varsityTable = document.getElementById('varsity-table');
 const jhTable = document.getElementById('jh-table');
 
-toggle.addEventListener('change', () => {
-    if (toggle.checked) {
-        varsityTable.style.display = 'none';
-        jhTable.style.display = 'block';
-    } else {
-        varsityTable.style.display = 'block';
-        jhTable.style.display = 'none';
+// toggle.addEventListener('change', () => {
+//     if (toggle.checked) {
+//         varsityTable.style.display = 'none';
+//         jhTable.style.display = 'block';
+//     } else {
+//         varsityTable.style.display = 'block';
+//         jhTable.style.display = 'none';
+//     }
+// })
+
+if (toggle && varsityTable && jhTable) {
+    // Add both change and focusout events
+    toggle.addEventListener('change', updateToggle);
+    toggle.addEventListener('focusout', updateToggle);
+
+    function updateToggle() {
+        if (toggle.checked) {
+            varsityTable.style.display = 'none';
+            jhTable.style.display = 'block';
+        } else {
+            varsityTable.style.display = 'block';
+            jhTable.style.display = 'none';
+        }
     }
-})
+}
+
 
 
